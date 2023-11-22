50889b850ba3971236ff00b846acd2b41b7e9ae2
Minh Nhật <89580650+L3oN007@users.noreply.github.com>
2023-11-22 12:59:40 +0700

    Done server action for delete and copy list

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-header.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-header.tsx
@@ -7,6 +7,7 @@ import { List } from '@prisma/client';
 import { useState, useRef, ElementRef } from 'react';
 import { toast } from 'sonner';
 import { useEventListener } from 'usehooks-ts';
+import { ListOptions } from './list-options';
 interface ListHeaderProps {
 	data: List;
 }
@@ -107,6 +108,8 @@ export const ListHeader = ({ data }: ListHeaderProps) => {
 					{title}
 				</div>
 			)}
+
+			<ListOptions data={data} onAddCard={() => {}} />
 		</div>
 	);
 };
